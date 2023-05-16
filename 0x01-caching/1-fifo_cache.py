4aabbca4ef054ac703d1e17f296d690b0d765736
Joshdb-18 <joshuadami17@gmail.com>
2023-05-16 13:36:08 +0100

    FIFO Caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Task 1
+"""
+
+from base_caching import BaseCaching
+
+
+
+class FIFOCache(BaseCaching):
+    """
+    Inherits from BaseCahing and is a caching system
+    """
+    def __init__(self):
+        """
+        Caling parent init
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        Assigns to a dict self.cache_data the item value for the
+        key <key>
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            if self.queue:
+                discarded_key = self.queue.pop(0)
+                del self.cache_data[discarded_key]
+                print("DISCARD: {}".format(discarded_key))
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """
+        Returns the value in self.cache_data linked to key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
